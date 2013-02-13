375ecd73ee855527b1c27c27f25eede5c0f9f919
Jay Kruer <jaykru@me.com>
2013-02-12 23:28:19 -0500

    Solenoid code and housecleaning
    Added @aminnich's solenoid test code and the launcher code. Also,
    removed WPILib to avoid redundancy. Lastly, added a gitignore file to
    get rid of these infuriating .DS_Store files.

--- src/launcher.cc
+++ src/launcher.cc
@@ -1,34 +1,20 @@
 #include "WPILib.h"
-#include "math.h"
 
-class ShooterDemo : public SimpleRobot {
-	RobotDrive myRobot; // Robot drive system
-	Joystick lstick; // Front wheel throttle.
-	Joystick rstick; // Rear wheel throttle.
-	Jaguar fwheel; // Front wheel.
-	Jaguar rwheel; // Rear wheel.
+class Launcher : public SimpleRobot {
+	Jaguar fwheel; // Create Jaguar instance for the front wheel.
+	Jaguar rwheel; // Creat Jaguar instance for the rear wheel.
 public:
-	ShooterDemo(void):
-		lstick(1),
-		rstick(2),
-		fwheel(9), // Front wheel set to port 9 on Jaguar.
-		rwheel(5), // Rear wheel set to port 5 on Jaguar.
+	Launcher(void):
+    fwheel(8), // Set front wheel to port 8 on Jaguar.
+    rwheel(9) // Set rear wheel to port 9 on Jaguar.
+	{}
 	void OperatorControl(void) {
-		bool fwstate = 0; // State of front wheel, off.
-		bool rwstate = 0; // State of rear wheel, off.
-		int fwdir = 1; // Direction of the front wheel.
-		int rwdir = 1; // Direction of the rear wheel.
-		
 		while (IsOperatorControl())
 		{
-			SmartDashboard::PutNumber("Shoot wheel 1", 0.5 * (rightstick.GetThrottle() + 1));
-			SmartDashboard::PutNumber("Shoot wheel 2", 0.5 * (leftstick.GetThrottle() + 1));
-			if (rstick.GetThrottle()) {
-				fwheel.Set(fwdir * 0.5 * (rstick.GetThrottle() + 1));
-			}
-			else if (lstick.GetThrottle()) {
-				rwheel.Set(rwdir * 0.5 * (lstick.GetThrottle() + 1));
-			}
+			fwheel.Set(1.0); // Set one motor to full capacity.
+			rwheel.Set(0.9); // Set one motor to 90% capacity.
 		}
 	}
-}
+};
+
+START_ROBOT_CLASS(Launcher);
